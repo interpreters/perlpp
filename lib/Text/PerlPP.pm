b2110560ce910735330a07c9515f70898abec97a
Chris White <chrisw@leehayes.com>
2018-05-11 18:46:47 -0400

    Cleanup to prep for release
    - `clean` target also deletes ./fatlib
    - Remove `Makefile-premodule` now that it has been integrated into
      Makefile.PL
    - Fill in Changes
    - Update installation instructions in README

--- lib/Text/PerlPP.pm
+++ lib/Text/PerlPP.pm
@@ -144,7 +144,7 @@ sub GetModeOfOB {
 
 sub DQuoteString {	# wrap $_[0] in double-quotes, escaped properly
 	# Not currently used by PerlPP, but provided for use by scripts.
-    # TODO? inject into the generated script?
+	# TODO? inject into the generated script?
 	my $s = shift;
 
 	$s =~ s{\\}{\\\\}g;
